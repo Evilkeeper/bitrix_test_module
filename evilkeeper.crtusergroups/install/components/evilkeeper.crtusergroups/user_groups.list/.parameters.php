0e29dbeff1ac6013833dac462e9a5138e15263cc
nem <nem@crtweb.ru>
2018-07-30 14:57:17 +0500

    modified class usage

--- evilkeeper.crtusergroups/install/components/evilkeeper.crtusergroups/user_groups.list/.parameters.php
+++ evilkeeper.crtusergroups/install/components/evilkeeper.crtusergroups/user_groups.list/.parameters.php
@@ -1,10 +1,12 @@
 <?php
+use \Bitrix\Main\Localization\Loc;
+
 $arComponentParameters = [
     'PARAMETERS' => [
         'CACHE_TIME' => [],
         'SET_TITLE' => [],
         'NEW_TITLE' => [
-            'NAME' => \Bitrix\Main\Localization\Loc::getMessage('NEW_TITLE')
+            'NAME' => Loc::getMessage('NEW_TITLE')
         ],
     ]
 ];
